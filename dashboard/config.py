9f785195b1e15926c4bc1bea90a52e00a1b4af5a
bperdomo2016 <77758553+bperdomo2016@users.noreply.github.com>
2021-08-15 01:06:29 -0400

    Create config.py

---
+++ dashboard/config.py
@@ -0,0 +1,12 @@
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
+
+ct_users = [
+    '22loops',
+    'econoar',
+    'CryptoDonAlt',
+    'zhusu',
+    'ColdBloodShill'
+]
