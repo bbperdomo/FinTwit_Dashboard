e1cf90de22cdc5f412545702c97084de79b0a507
bperdomo2016 <bperdomo2016@fau.edu>
2021-08-21 02:40:40 -0400

    "added regex,dict,df"

--- dashboard/backend.py
+++ dashboard/backend.py
@@ -47,4 +47,50 @@ fintwit_users = [
 ]
 
 #list comprehension to flex
-all_tweets = [api.user_timeline(screen_name=user_handle, count=200,lang='en',tweet_mode='extended') for user_handle in fintwit_users]
+all_tweets = [api.user_timeline(screen_name=user_handle, count=10,lang='en',tweet_mode='extended') for user_handle in fintwit_users]
+
+#uses regex to identify tweets containing a stock ticker
+pattern = "\$[a-zA-Z+]"
+cashtag_tweets = []
+
+for tweet_list in all_tweets:
+  for tweet in tweet_list:
+    text = tweet._json["full_text"]
+    if re.findall(pattern,text):
+      cashtag_tweets.append(text)
+
+
+# Building a stock dictionary
+cashtag_dict = {}
+
+#for tweet in posts[:100]:
+for tweet in cashtag_tweets:
+  #text = tweet._json["full_text"]
+  #breaks up strings into words
+  splits = tweet.split()
+
+  for word in splits:
+    #if pattern is found in a word
+    if re.findall(pattern,word):
+      #strip erroneous chars, and keeps $. Do I really need it?
+      #word = word.strip(",.?!'[]@()*")
+      #Completely cleans all tickers, but removes $. Probably better
+      word = re.sub(r'[^a-zA-Z0-9]', '', word)
+      if word in cashtag_dict:
+        cashtag_dict[word] += 1
+      else:
+        cashtag_dict[word] = 1
+#print(cashtag_dict)
+
+#Init a dataframe and populate with symbol and frequency
+tweet_df = pd.DataFrame(cashtag_dict.items(),columns=['Symbol','Frequency'])
+
+#sorted_df
+descending_df = tweet_df.sort_values(by=['Frequency'],ascending=False)
+#print(descending_df.to_string(index=False))
+
+#printing top 5 from sorted df
+print(descending_df[0:5].to_string(index=False))
+
+#create top 5 dataframe
+top_df = descending_df[0:5]
